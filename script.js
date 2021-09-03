ad39d6e1480e933dd8a33b04fe0cbebae516b433
jonathan653 <81852222+jonathan653@users.noreply.github.com>
2021-09-03 13:46:12 +1200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+init();
+
+function init() {
+    var url = "https://api.covid19api.com/summary";
+    var data = "";
+    $.get(url, function (data) {
+        data = `
+            <td>${data.Global.NewConfirmed}</td>
+            <td>${data.Global.TotalConfirmed}</td>
+            <td>${data.Global.NewDeaths}</td>
+            <td>${data.Global.TotalDeaths}</td>
+            <td>${data.Global.TotalRecovered}</td>
+            `
+        $("#data").html(data);
+    })
+}
+
+function refreshData() {
+    clearData();
+    init();
+}
+
+function clearData() {
+    $("#data").empty();
+}
